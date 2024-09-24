741f1e7df1005baed1d5f60df92a623d4c9f44ff
Bobbie Janssen <174934975+BobbieJanssen@users.noreply.github.com>
2024-09-24 10:37:35 +0200

    Translator now works with the specified  arrays, but I have to add more for the assignment.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 
 public class Main {
     public static void main(String[] args) {
+        Integer[] numeric = {1, 2, 3 ,4 ,5 ,6 ,7 ,8, 9, 0};
+        String[] alphabetic = {"een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "nul"};
 
-        // Hier mag je je code schrijven voor de hoofd-opdracht
+        Translator translator = new Translator (alphabetic, numeric);
 
-
+        System.out.println(translator.numericAlpha);
     }
 }
